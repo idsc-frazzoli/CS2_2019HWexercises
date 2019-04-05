60aa250e2024aea6981eadc06600e9d6d621a42f
lorenzmei <46973437+lorenzmei@users.noreply.github.com>
2019-04-05 15:12:44 +0200

    Update controller-1.py

--- Exercises/HWExercise3/controller-1.py
+++ Exercises/HWExercise3/controller-1.py
@@ -11,18 +11,18 @@ class Controller():
 
         #TODO
 
-    # Inputs:   d_est   Estimation of distance from lane center (positve when
-    #                   offset to the left of driving direction) [m]
-    #           phi_est Estimation of angle of bot (positive when angle to the
-    #                   left of driving direction) [rad]
-    #           d_ref   Reference of d (for lane following, d_ref = 0) [m]
-    #           v_ref   Reference of velocity [m/s]
-    #           t_delay Delay it took from taking image up to now [s]
-    #           dt_last Time it took from last processing to current [s]
+        # Inputs:   d_est   Estimation of distance from lane center (positve when
+        #                   offset to the left of driving direction) [m]
+        #           phi_est Estimation of angle of bot (positive when angle to the
+        #                   left of driving direction) [rad]
+        #           d_ref   Reference of d (for lane following, d_ref = 0) [m]
+        #           v_ref   Reference of velocity [m/s]
+        #           t_delay Delay it took from taking image up to now [s]
+        #           dt_last Time it took from last processing to current [s]
 
-    # Output:   v_out       velocity of Duckiebot [m/s]
-    #           omega_out   angular velocity of Duckiebot [rad/s]
-
+        # Output:   v_out       velocity of Duckiebot [m/s]
+        #           omega_out   angular velocity of Duckiebot [rad/s
+        
     def getControlOutput(self, d_est, phi_est, d_ref, phi_ref, v_ref, t_delay, dt_last):
         #Exception handler for dt_last==0
         if (dt_last==0) : dt_last=0.0001
